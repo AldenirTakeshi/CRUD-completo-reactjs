8db2732852aeedbbbcd332a5738f5c669442bc1b
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-18 08:53:08 -0400

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import Home from "./components/Home";
 import Alunos from "./components/Alunos";
 import Sobre from "./components/Sobre";
 import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
-
+import { Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
       <div className="container">
         <h1>Hello World!</h1>
         <BrowserRouter>
-          <ul>
-            <li>
-              <Link to="/">Página inicial</Link>
-            </li>
-            <li>
-              <Link to="/alunos">Alunos</Link>
-            </li>
-            <li>
-              <Link to="/sobre">Sobre</Link>
-            </li>
-          </ul>
-
+          <Nav variant="tabs">
+            <Nav.Link as={Link} to="/">
+              Página Inicial
+            </Nav.Link>
+            <Nav.Link as={Link} to="/alunos">
+              Alunos
+            </Nav.Link>
+            <Nav.Link as={Link} to="/sobre">
+              Sobre
+            </Nav.Link>
+          </Nav>
+          {/* Routes */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/alunos" element={<Alunos />} />
