53e1dd917b5274dc8d6b8a12d42e05a345efa0cb
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-18 10:07:33 -0400

    add table

--- src/App.js
+++ src/App.js
@@ -8,28 +8,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <div className="container">
-        <h1>Hello World!</h1>
-        <BrowserRouter>
-          <Nav variant="tabs">
-            <Nav.Link as={Link} to="/">
-              Página Inicial
-            </Nav.Link>
-            <Nav.Link as={Link} to="/alunos">
-              Alunos
-            </Nav.Link>
-            <Nav.Link as={Link} to="/sobre">
-              Sobre
-            </Nav.Link>
-          </Nav>
-          {/* Routes */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/alunos" element={<Alunos />} />
-            <Route path="/sobre" element={<Sobre />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <h1>Hello World!</h1>
+      {/* Links */}
+      <BrowserRouter>
+        <Nav variant="tabs">
+          <Nav.Link as={Link} to="/">
+            Página Inicial
+          </Nav.Link>
+          <Nav.Link as={Link} to="/alunos">
+            Cadastro de Alunos
+          </Nav.Link>
+          <Nav.Link as={Link} to="/sobre">
+            Sobre
+          </Nav.Link>
+        </Nav>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/alunos" element={<Alunos />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,7 +1,42 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import { Table } from "react-bootstrap";
 
-const Alunos = () => {
-  return <div>Alunos</div>;
-};
+class Alunos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alunos: [],
+    };
+  }
 
+  render() {
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Opção</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>Atualizar Excluir</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>Atualizar Excluir</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  }
+}
 export default Alunos;
