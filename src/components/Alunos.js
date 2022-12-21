101654d71421dfd38f4d23074e141e0fb3724a48
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-21 08:06:11 -0400

    add GET Atualizar Userss

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -5,6 +5,7 @@ class Alunos extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 0,
       nome: "",
       email: "",
       alunos: [],
@@ -35,6 +36,28 @@ class Alunos extends React.Component {
     );
   };
 
+  carregarDados = (id) => {
+    fetch("http://localhost:3333/users/" + id, { method: "GET" })
+      .then((res) => res.json())
+      .then((aluno) => {
+        this.setState({ id: aluno.id, nome: aluno.nome, email: aluno.email });
+      });
+  };
+
+  cadastraAluno = (aluno) => {
+    fetch("http://localhost:3333/users/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(aluno),
+    }).then((res) => {
+      if (res.ok) {
+        this.buscarAlunos();
+      } else {
+        alert("Não foi possivel adicionar o aluno!");
+      }
+    });
+  };
+
   atualizaNome = (e) => {
     this.setState({
       nome: e.target.value,
@@ -46,13 +69,25 @@ class Alunos extends React.Component {
     });
   };
 
+  submit = () => {
+    const aluno = {
+      nome: this.state.nome,
+      email: this.state.email,
+    };
+    this.cadastraAluno(aluno);
+  };
   render() {
     return (
       <>
         <div style={{ margin: "1em 1em" }}>
           <Form>
             <Form.Group className="mb-3">
-              <Form.Label>Usuário</Form.Label>
+              <Form.Label>ID:</Form.Label>
+              <Form.Control type="text" value={this.state.id} readOnly={true} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Usuário:</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Digite seu nome de usuário"
@@ -61,16 +96,17 @@ class Alunos extends React.Component {
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
+            <Form.Group className="mb-3">
+              <Form.Label>Email:</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="Digite seu email"
+                value={this.state.email}
                 onChange={this.atualizaEmail}
               />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={this.submit}>
               Salvar
             </Button>
           </Form>
@@ -87,12 +123,17 @@ class Alunos extends React.Component {
             </thead>
             <tbody>
               {this.state.alunos.map((aluno, indice) => (
-                <tr>
+                <tr key={indice}>
                   <td>{indice + 1}</td>
                   <td>{aluno.nome}</td>
                   <td>{aluno.email}</td>
                   <td>
-                    <Button variant="success">Success</Button>{" "}
+                    <Button
+                      variant="success"
+                      onClick={() => this.carregarDados(aluno.id)}
+                    >
+                      Atualizar
+                    </Button>{" "}
                     <Button
                       variant="danger"
                       onClick={() => this.deletarAluno(aluno.id)}
